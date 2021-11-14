90f8b4687efe3d50d93c890cec666fa2c528e70d
Chris Wiker <cwiker7@gmail.com>
2021-11-14 13:09:24 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solutions in this file
+count = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+
+function countDown(count) {
+    for (let count = 10; count > 0; count--);
+    console.log(count);
+}
