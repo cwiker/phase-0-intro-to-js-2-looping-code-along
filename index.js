4b6562a0730c333adfeea500110958e811d7affa
Chris Wiker <cwiker7@gmail.com>
2021-11-26 23:23:37 -0500

    commit tree e58cb8ac9f4732a44cda48e05c3e51e06c9f2f82

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+const array = [];
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+    array.push(`Thank you, ${names[i]}, for the wondeful ${event} gift!`);
+    debugger;}
+    return array;
+}
+
+
+
 count = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 
 function countDown(count) {
-    for (let count = 10; count > 0; count--);
-    console.log(count);
+    for (let i = 10; i > count.length; i--);
+    console.log(count[i]);
+    
 }
